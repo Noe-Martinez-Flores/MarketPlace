ffc0a6601a34e817410451186720a12a92292dd6
Noe-Martinez-Flores <20203tn035@utez.edu.mx>
2022-01-25 13:52:40 -0600

    first commit

---
+++ src/test/java/mx/edu/utez/MarketPlace/MarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.MarketPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
